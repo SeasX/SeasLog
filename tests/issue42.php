fedb5d6ffe1219406bd7419b1c96019ec301c31e
neeke <ciogao@gmail.com>
2015-09-14 21:36:20 +0800

    update ttests issue42

---
+++ tests/issue42.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @author ciogao@gmail.com
+ * Date: 15-9-14 下午9:35
+ */
+
+function inverse($x) {
+    if (!$x) {
+        throw new Exception('Division by zero.');
+    }
+    return 1/$x;
+}
+
+try {
+    echo inverse(5) . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+} finally {
+    echo "First finally.\n";
+}
+
+try {
+    echo inverse(0) . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+} finally {
+    echo "Second finally.\n";
+}
+
+// Continue execution
+echo "Hello World\n";
