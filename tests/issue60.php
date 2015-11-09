a85788fb6c25e2b10b354f8bce76dd9b966227b4
neeke <ciogao@gmail.com>
2015-11-09 19:55:33 +0800

    add testcase for issue #60

---
+++ tests/issue60.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author neeke@php.net
+ * Date: 15-11-9 下午7:16
+ */
+
+for($i=0; $i<100; $i++){
+    SeasLog::error(time());
+    sleep(1);
+
+    var_dump(count(SeasLog::getBuffer()));
+}
