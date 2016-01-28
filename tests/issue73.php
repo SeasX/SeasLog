a45e729bcce2969f9d4738d1a2ce0db33072bb62
neeke <neeke@php.net>
2016-01-28 23:24:06 +0800

    add issue73 test

---
+++ tests/issue73.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author neeke@php.net
+ * Date: 16/1/11 上午10:15
+ */
+function testAction(){
+    $path = "/usr/local/nginx/html/logs";
+    \SeasLog::setBasePath($path);
+    \SeasLog::setBasePath($path);
+    \SeasLog::notice("a");
+    return;
+}
+
+testAction();
