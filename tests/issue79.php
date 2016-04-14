535291a0c03601c1d669ffe7722a378a367a2436
neeke <neeke@php.net>
2016-04-14 22:29:31 +0800

    add issue 79 test

---
+++ tests/issue79.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @author neeke@php.net
+ * Date: 16/4/14 下午9:57
+ */
+class testBasePathAndLogger
+{
+    public function __construct()
+    {
+
+    }
+
+    public function TestRun()
+    {
+        $this->testBasePath();
+        $this->testLogger();
+    }
+
+    private function testBasePath()
+    {
+        SeasLog::setBasePath('aaa');
+        SeasLog::setBasePath('bbb');
+    }
+
+    private function testLogger()
+    {
+        SeasLog::setLogger('aaa');
+        SeasLog::setLogger('bbb');
+    }
+}
+
+$testObj = new testBasePathAndLogger();
+$testObj->TestRun();
