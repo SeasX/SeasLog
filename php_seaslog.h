d4b8854d6655b09e7fa8343b6bc7b7cdf03c5c80
neeke <neeke@php.net>
2016-05-26 01:07:25 +0800

    update to 1.5.8 beta

--- php_seaslog.h
+++ php_seaslog.h
@@ -33,7 +33,7 @@ extern zend_module_entry seaslog_module_entry;
 #endif
 
 #define SEASLOG_RES_NAME                    "SeasLog"
-#define SEASLOG_VERSION                     "1.5.6"
+#define SEASLOG_VERSION                     "1.5.8 - beta"
 #define SEASLOG_AUTHOR                      "Chitao.Gao  [ neeke@php.net ]"
 
 #define SEASLOG_ALL                         "all"
