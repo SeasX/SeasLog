e255c5404036ad10861ad8d72e4c1298c55666da
neeke <neeke@php.net>
2017-07-28 16:32:24 +0800

    Add test case for #110

---
+++ tests/issue110.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author ciogao@gmail.com
+ * Date: 16/12/6 下午8:08
+ */
+$logger_1 = new SeasLog();
+
+
+$logger_2 = new SeasLog();
+
+$logger_1->aaa = 1111;
+$logger_2->aaa = 2222;
+
+var_dump($logger_1->aaa);
+
+var_dump($logger_2);
