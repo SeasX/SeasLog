c6fd27a84d5efdcf6e077ddced733508f094c363
neeke <neeke@php.net>
2018-03-19 16:49:58 +0800

    update to 1.8.4

--- include/SeasLog.h
+++ include/SeasLog.h
@@ -39,7 +39,7 @@
 
 #define SEASLOG_RES_NAME                    "SeasLog"
 #define SEASLOG_AUTHOR                      "Chitao.Gao  [ neeke@php.net ]"
-#define SEASLOG_VERSION                     "1.8.3"
+#define SEASLOG_VERSION                     "1.8.4"
 #define SEASLOG_VERSION_ID                  10800
 #define SEASLOG_SUPPORTS                    "https://github.com/SeasX/SeasLog"
 
