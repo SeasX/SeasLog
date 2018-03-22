dc4a0d9b4bebecbebcb7fb4878ae39b920b6c23e
neeke <neeke@php.net>
2018-03-22 20:56:57 +0800

    update test case

--- tests/seaslog.test.php
+++ tests/seaslog.test.php
@@ -18,6 +18,8 @@ SeasLog::debug('this is a {userName} debug', array('{userName}' => 'neeke'));
 
 SeasLog::info('this is a info log');
 
+SeasLog::info('this is a info log',array(),'logger_info_test');
+
 SeasLog::notice('this is a notice log');
 
 SeasLog::warning('your {website} was down,please {action} it ASAP!', array('{website}' => 'github.com', '{action}' => 'rboot'));
@@ -34,8 +36,8 @@ var_dump(SeasLog::analyzerCount(SEASLOG_INFO, date('Ymd', time())));
 var_dump(SeasLog::analyzerCount(SEASLOG_INFO));
 var_dump(SeasLog::analyzerCount());
 var_dump(SeasLog::analyzerDetail(SEASLOG_INFO, date('Ymd', time())));
-var_dump(SeasLog::analyzerDetail(SEASLOG_DEBUG, date('Ymd', time()), 'neeke', 1, 40));
-var_dump(SeasLog::analyzerDetail(SEASLOG_DEBUG, date('Ymd', time()), 'neeke', 1, 40, SEASLOG_DETAIL_ORDER_DESC));
+var_dump(SeasLog::analyzerDetail(SEASLOG_DEBUG, date('Ymd', time()), 'neeke', 1, 10));
+var_dump(SeasLog::analyzerDetail(SEASLOG_DEBUG, date('Ymd', time()), 'neeke', 1, 10, SEASLOG_DETAIL_ORDER_DESC));
 var_dump(SeasLog::getLastLogger());
 
 $buffer = SeasLog::getBuffer();
