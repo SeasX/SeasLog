a6a720d03e7386dbe222f26daefafcf831cc62cc
neeke <neeke@php.net>
2019-02-11 12:30:18 +0800

    update to 2.0.2

--- include/SeasLog.h
+++ include/SeasLog.h
@@ -39,8 +39,8 @@
 
 #define SEASLOG_RES_NAME                        "SeasLog"
 #define SEASLOG_AUTHOR                          "Chitao.Gao  [ neeke@php.net ]"
-#define SEASLOG_VERSION                         "2.0.1"
-#define SEASLOG_VERSION_ID                      20001
+#define SEASLOG_VERSION                         "2.0.2"
+#define SEASLOG_VERSION_ID                      20002
 #define SEASLOG_SUPPORTS                        "https://github.com/SeasX/SeasLog"
 
 #define SEASLOG_ALL                             "ALL"
