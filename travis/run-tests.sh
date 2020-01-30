c1940270b7bd28f3dc80ae45fb27e9591db722e6
Tinggong <wangtinggong@gmail.com>
2020-01-30 16:49:24 +0800

    show output files on tests failed

--- travis/run-tests.sh
+++ travis/run-tests.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 TEST_DIR="`pwd`/tests/"
 
-TEST_PHP_ARGS="-m --show-diff" make test
+TEST_PHP_ARGS="-m --show-diff -q" make test
 
-for file in `find $TEST_DIR -name "*.diff" 2>/dev/null`
+EXIT_STATUS=0
+
+for file in `find $TEST_DIR -type f ! -name '*.phpt' -and ! -name '*.php' -and ! -name '*.png'`
 do
-	grep "\-\-XFAIL--" ${file/%diff/phpt} >/dev/null 2>&1
-	if [ $? -gt 0 ]
-	then
-		FAILS[${#FAILS[@]}]="$file"
-	fi
+    echo '====================================================================='
+    sh -xc "cat $file"
+    EXIT_STATUS=1
 done
 
-if [ ${#FAILS[@]} -gt 0 ]
-then
-	for fail in "${FAILS[@]}"
-	do
-		sh -xc "cat $fail"
-	done
-	exit 1
-else
-	exit 0
-fi
+exit $EXIT_STATUS
